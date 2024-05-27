8637181ed717f04ce34913c2dba27ae089bf1e50
delfisolis <164261606+delfi21@users.noreply.github.com>
2024-05-27 13:59:18 -0300

    Add files via upload

--- guia7.py
+++ guia7.py
@@ -1,3 +1,5 @@
+
+import random
 #problema2 :: suma de elemenos de una lista 
 def suma_total(x: list[int]):
     suma: int = 0
@@ -14,7 +16,7 @@ def suma_total2(x: list[int]):
         suma += i
     return suma 
 
-print(suma_total([1,1]))
+
 
 #problema1 :: pertenece
 
@@ -33,7 +35,7 @@ def tiene_mayuscula(x:str) -> bool:
     for i in x:
         if i>='A' and i<='Z' and not es_mayuscula: 
             es_mayuscula=True
-    return es_mayuscula
+    return es_mayuscula3
 
 def tiene_minuscula(x:str) -> bool:
     es_minuscula: bool =False  
@@ -61,6 +63,61 @@ def pertenece_a_cada_uno_version_2(x:list[list[int]], n: int, res:list[bool]):
     res.clear()
     for i in range(len(x)) :
         res.append (pertenece(x[i],n))
+
+# Ejercicio 4
+def lista_nombres()->list[str]:
+    lista_nombres:list[str] = []
+    sigo_pidiendo: bool = True 
+    while sigo_pidiendo:
+        nombre=input("Ingrese nombre:\n")
+        if nombre == "listo":
+            sigo_pidiendo= False 
+        else:
+            lista_nombres.append(nombre)
+    return(lista_nombres)
+
+def historial_monedero() -> list[tuple]:
+    historial_monedero:list[int] = []
+    continuar_operacion:bool = True 
+    while continuar_operacion:
+        operacion=input("seleccione la operacion a realizar:\n")
+        if operacion == "C":
+            monto_a_cargar:int=int(input("ingrese el monto a cargar:\n"))
+            historial_monedero.append(("C", monto_a_cargar))
+        elif operacion == "D":
+              monto_a_descontar:int=int(input("ingrese el monto a descontar:\n"))
+              historial_monedero.append(("D", monto_a_descontar))
+        elif operacion == "X":
+            continuar_operacion= False 
+    return historial_monedero 
+
+
+
+def siete_y_medio()-> list[int]: 
+    historial_cartas:list[int] = []
+    puntos:list[int] = []
+    seguir_jugando= True 
+    while seguir_jugando:
+        carta: int = random.choice([1,2,3,4,5,6,7,10,11,12])
+        historial_cartas.append(carta)
+        if carta <= 7:
+            puntos.append(carta)
+        else:
+            puntos.append(0.5)
+        if sum(puntos) >= 7.5:
+            print("¡Perdiste!")
+            seguir_jugando=False 
+        else:
+            print(carta)
+            dar_otra_carta= input("¿queres sacar otra carta?:\n") 
+            if dar_otra_carta == "no":
+                seguir_jugando = False 
+                print(f"Ganaste {sum(puntos)}")
+                
+    return historial_cartas 
+
+print(siete_y_medio())
+
 
 
 
